173b6210a813016e83bcea7c373053bc3f062a9c
miranda <miranda.wqa@gmail.com>
2026-02-22 20:53:58 -0500

    rushing the rest run

--- tests/playwright/test-1.spec.ts
+++ tests/playwright/test-1.spec.ts
@@ -5,25 +5,25 @@ test('File 1: Test 1', async ({ page }) => {
   console.log(`${test.info().status?.toUpperCase()}`);
 });
 
-test('File 1: Test 2', async ({ page }) => {
-  await expect(page).toHaveTitle(/Playwright/);
-  console.log(`${test.info().status?.toUpperCase()}`);
-});
+// test('File 1: Test 2', async ({ page }) => {
+//   await expect(page).toHaveTitle(/Playwright/);
+//   console.log(`${test.info().status?.toUpperCase()}`);
+// });
 
-test('File 1: Test 3', async ({ page }) => {
-  await expect(page).toHaveTitle(/Playwright/);
-  console.log(`${test.info().status?.toUpperCase()}`);
-});
+// test('File 1: Test 3', async ({ page }) => {
+//   await expect(page).toHaveTitle(/Playwright/);
+//   console.log(`${test.info().status?.toUpperCase()}`);
+// });
 
-test('File 1: Test 4', async ({ page }) => {
-  await expect(page).toHaveTitle(/Playwright/);
-  console.log(`${test.info().status?.toUpperCase()}`);
-});
+// test('File 1: Test 4', async ({ page }) => {
+//   await expect(page).toHaveTitle(/Playwright/);
+//   console.log(`${test.info().status?.toUpperCase()}`);
+// });
 
-test.skip('File 1: Test 5', async ({ page }) => {
-  await expect(page).toHaveTitle(/Playwright/);
-  console.log(`${test.info().status?.toUpperCase()}`);
-});
+// test.skip('File 1: Test 5', async ({ page }) => {
+//   await expect(page).toHaveTitle(/Playwright/);
+//   console.log(`${test.info().status?.toUpperCase()}`);
+// });
 
 test('File 1: Test 6', async ({ }) => {
   throw new Error("This test failed on purpose!");
